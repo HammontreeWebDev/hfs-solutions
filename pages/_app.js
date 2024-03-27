70fd41fb1547eb6547c6a053928f5eb3ba92e529
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 17:03:11 -0400

    added comments and sub-text css variable

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,34 @@
 import Head from "next/head";
 import "../styles/global.css";
-import { Architects_Daughter } from 'next/font/google';
 
+// import fonts from next/font/google to avoid making requests from google
+import { Architects_Daughter } from 'next/font/google';
+import { Josefin_Sans } from "next/font/google";
+
+// store fonts in a variable and specify parameters
 const architectsDaughter = Architects_Daughter({
     subsets: ['latin'],
     preload: true,
     weight: ['400'],
 });
 
+const josefinSans = Josefin_Sans({
+    subsets: ['latin'],
+    preload: true,
+    weight: ['400'],
+})
+
 const App = ({ Component, pageProps }) => {
     return (
         <>
+
+        {/* add fonts to global application to be stored as variables that can be called in any css file */}
         <style jsx global>
             {
                 `
                 :root {
                     --main-text: ${architectsDaughter.style.fontFamily};
+                    --sub-text: ${josefinSans.style.fontFamily};
                 }
                 `
             }
