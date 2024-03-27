07da521408cce2456acee38767d44037a476d50b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 18:17:15 -0400

    added Header component boilerplate

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+    return(
+        // classname for dimension specifications
+        <header className={styles.container}>
+            <p>This is header text test</p>
+        </header>
+    )
+}
