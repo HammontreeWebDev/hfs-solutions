808c8c1e7e70e741d2ddc4a2b2a545d0c2270d24
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-28 10:14:28 -0400

    changed flex behavior for rows to justify content based on space between and created columns for the titleimage modulse

--- components/TitleImage.js
+++ components/TitleImage.js
@@ -1,8 +1,20 @@
-export default function TitleImage(props){
-    return(
+import styles from '../styles/TitleImage.module.css';
 
-        <div>
-            <p>this is test text</p>
-        </div>
+export default function TitleImage(props) {
+    return (
+
+        <>
+
+            {/* Left Side: Title Text */}
+            <div className={styles.column}>
+
+            </div>
+
+            {/* Right Side: Image */}
+            <div className={styles.column}>
+
+            </div>
+
+        </>
     )
 }
