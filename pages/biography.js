15fa0d102d64621991c9457bb4b5b797cf8a367d
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 21:42:50 -0400

    boiler for bio page

---
+++ pages/biography.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+
+export default function Biography() {
+
+    return (
+        <>
+
+            <Head>
+                <title>Bio | Kaileb Hammontree</title>
+            </Head>
+
+            <Header />
+
+            <main>
+
+            <p>This is the Bio Page</p>
+
+            </main>
+            
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>KailebHammontree.com</title>
+        <title>Home | Kaileb Hammontree</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-    </div>
+    </>
   );
 }
