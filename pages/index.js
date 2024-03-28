0885f3c2a5c325e6dd06df829a45822f1172041a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-28 10:44:02 -0400

    added comments to home page to indicate where components will go

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,17 @@ export default function Home() {
       <main>
         <RowSectionContainer jsxContent={
 
+          // !Row 1 - TitleImage Component //
           <TitleImage
             titleText={`Transforming Visions Into Digital Reality`}
             imageSrc={`../img/home-page/home-page-mock-up.svg`}
           />
+
+          // !Row 2 TitleCarousel Component (normal) //
+
+          // !Row 3 TitleCarousel Component (row-reverse) //
+
+          // !Row 4 Title Carousel Component (normal) //
 
         } />
       </main>
