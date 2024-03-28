bc02148aff096a121e20a83498dbacfce2f11b39
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 20:50:05 -0400

    added header vector

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
 import styles from '../styles/Header.module.css';
+import Image from 'next/image';
 
 export default function Header() {
     return(
         // classname for dimension specifications
         <header className={styles.container}>
-            <p>This is header text test</p>
+
+            <Image className={styles.leftImage} src='../img/Header/hfs-header-left.svg' width={578} height={430} />
+            <Image className={styles.vector} src='../img/Header/header-vector.svg' width={1376} height={130} />
+            {/* <p>This is header text test</p> */}
         </header>
     )
 }
