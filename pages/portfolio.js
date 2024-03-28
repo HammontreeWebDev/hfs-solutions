590c6a6133fc6ffb1bba4a31c083942b39075929
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 21:44:53 -0400

    set up boiler for portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+
+export default function Portfolio() {
+
+    return (
+        <>
+
+            <Head>
+                <title>Portfolio | Kaileb Hammontree</title>
+            </Head>
+
+            <Header />
+
+            <main>
+
+            <p>This is the Portfolio Page</p>
+
+            </main>
+            
+        </>
+    )
+}
