e5bbcc4358d33e0e903ca3ed0b5ff532287fbfbf
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 21:44:11 -0400

    set up boiler for contact page

---
+++ pages/contact.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+
+export default function Contact() {
+
+    return (
+        <>
+
+            <Head>
+                <title>Contact | Kaileb Hammontree</title>
+            </Head>
+
+            <Header />
+
+            <main>
+
+            <p>This is the Contact Page</p>
+
+            </main>
+            
+        </>
+    )
+}
