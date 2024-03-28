07b08d454132f22941ade106b6ab299e44f88e5a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 21:43:33 -0400

    set up boiler for certifications

---
+++ pages/certifications.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+
+export default function Certifications() {
+
+    return (
+        <>
+
+            <Head>
+                <title>Certifications | Kaileb Hammontree</title>
+            </Head>
+
+            <Header />
+
+            <main>
+
+            <p>This is the Certifications Page</p>
+
+            </main>
+            
+        </>
+    )
+}
