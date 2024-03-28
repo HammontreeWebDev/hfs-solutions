877933ced6c07d46897d1f237688e150d7d1649f
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-27 21:31:51 -0400

    Added styling to buttons and next link added

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,20 @@
+import Link from 'next/link';
 import styles from '../styles/Header.module.css';
 import Image from 'next/image';
 
 export default function Header() {
-    return(
+    return (
         // classname for dimension specifications
         <header className={styles.container}>
-
             <Image className={styles.leftImage} src='../img/Header/hfs-header-left.svg' width={578} height={430} />
             <Image className={styles.vector} src='../img/Header/header-vector.svg' width={1376} height={130} />
-            {/* <p>This is header text test</p> */}
+            <div className={styles.navRow}>
+                <Link className={`${styles.navLink} subText`} href='/'>Home</Link>
+                <Link className={`${styles.navLink} subText`} href='/biography'>Biography</Link>
+                <Link className={`${styles.navLink} subText`} href='/contact'>Contact</Link>
+                <Link className={`${styles.navLink} subText`} href='/portfolio'>Portfolio</Link>
+                <Link className={`${styles.navLink} subText`} href='/certifications'>Certifications</Link>
+            </div>
         </header>
     )
 }
