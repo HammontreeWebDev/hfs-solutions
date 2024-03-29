71390f51c8f24417af5638e174b896d134efeaa6
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-29 16:19:16 -0400

    all footer links functional

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import styles from "../styles/Footer.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
     return (
@@ -23,18 +24,41 @@ export default function Footer() {
 
                     <div className={styles.subRow}>
 
-                        <Image src={'../img/Footer/linkedin.svg'} width={281} height={70} />
+                        <Link href={'https://www.linkedin.com/in/kaileb-hammontree/'}>
+                            <Image src={'../img/Footer/linkedin.svg'} width={281} height={70} alt="linked in logo" />
+                        </Link>
 
-                        <Image src={'../img/Footer/stackoverflow.svg'} width={71} height={70} />
+                        <Link href={' https://stackoverflow.com/users/20187731/hammontreewebdev?tab=profile'}>
+                            <Image src={'../img/Footer/stackoverflow.svg'} width={71} height={70} alt="stack overflow logo" />
+                        </Link>
 
-                        <Image src={'../img/Footer/github.svg'} width={197} height={70} />
+                        <Link href={'https://github.com/HammontreeWebDev'}>
+                            <Image src={'../img/Footer/github.svg'} width={197} height={70} alt="git hub logo" />
+                        </Link>
 
-                        <Image src={'../img/Footer/facebook.svg'} width={71} height={70} />
+                        <Link href={'https://www.facebook.com/hammontreefullstacksolutions/'}>
+                            <Image src={'../img/Footer/facebook.svg'} width={71} height={70} alt="facebook logo" />
+                        </Link>
 
                     </div>
 
-                    <div clasName={styles.subRow}>
+                    <div className={styles.subRow}>
+                        <Link className={`${styles.navLink} subText`} href='/'>
+                            Home
+                        </Link>
 
+                        <Link className={`${styles.navLink} subText`} href='/biography'>
+                            Biography
+                        </Link>
+                        <Link className={`${styles.navLink} subText`} href='/contact'>
+                            Contact
+                        </Link>
+                        <Link className={`${styles.navLink} subText`} href='/portfolio'>
+                            Portfolio
+                        </Link>
+                        <Link className={`${styles.navLink} subText`} href='/certifications'>
+                            Certifications
+                        </Link>
                     </div>
 
                 </div>
