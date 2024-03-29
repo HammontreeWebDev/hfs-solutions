8b1d0f0b0f8620cd1cf132dde8b8ea3dcb5a0013
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-29 16:01:15 -0400

    added footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,35 @@
 import styles from "../styles/Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
     return (
         <footer className={styles.container}>
             <div className={styles.row}>
 
-                <div className={styles.footerColumn}>
+                {/* insert image here */}
 
+                <div className={styles.footerColumn1}>
+                    <p className={styles.footerText}>
+                        Website Designed & Developed By:
+                        <br />
+                        Hammontree Full-Stack Solutions LLC
+                        <br />
+                        <br />
+                        Copyright © 2024. Hammontree Full-Stack Solutions LLC
+                    </p>
                 </div>
 
-                <div className={styles.footerColumn}>
+                <div className={styles.footerColumn2}>
 
                     <div className={styles.subRow}>
+
+                        <Image src={'../img/Footer/linkedin.svg'} width={281} height={70} />
+
+                        <Image src={'../img/Footer/stackoverflow.svg'} width={71} height={70} />
+
+                        <Image src={'../img/Footer/github.svg'} width={197} height={70} />
+
+                        <Image src={'../img/Footer/facebook.svg'} width={71} height={70} />
 
                     </div>
 
