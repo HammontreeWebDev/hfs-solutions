d3a91ecf46d130498eba55a40815bcbb29a92dbe
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-29 14:07:19 -0400

    added evt listener for collapsible nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,52 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/Header.module.css';
 import Image from 'next/image';
+import { LineMdCloseToMenuTransition } from './Icons/Menu';
 
 export default function Header() {
+
+    const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth <= 1235) {
+                setIsMobileDevice(true);
+            }
+            else {
+                setIsMobileDevice(false);
+            }
+        }
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+    }, []);
+
     return (
         // classname for dimension specifications
         <header className={styles.container}>
             <Image className={styles.leftImage} src='../img/Header/hfs-header-left.svg' width={578} height={430} />
             <Image className={styles.vector} src='../img/Header/header-vector.svg' width={1376} height={130} />
-            <div className={styles.navRow}>
+            {
+                isMobileDevice ?
+                <div className={styles.mobileRow}>
+
+                <LineMdCloseToMenuTransition />
+
+                </div>
+
+                :
+
+                <div className={styles.navRow}>
                 <Link className={`${styles.navLink} subText`} href='/'>Home</Link>
                 <Link className={`${styles.navLink} subText`} href='/biography'>Biography</Link>
                 <Link className={`${styles.navLink} subText`} href='/contact'>Contact</Link>
                 <Link className={`${styles.navLink} subText`} href='/portfolio'>Portfolio</Link>
                 <Link className={`${styles.navLink} subText`} href='/certifications'>Certifications</Link>
             </div>
+            }
         </header>
     )
 }
