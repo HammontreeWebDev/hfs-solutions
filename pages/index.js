9686c2fbe699e8d9eb6e776d6c6dfed6e73c80b4
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-30 16:38:31 -0400

    added all external links to icons

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,22 @@ export default function Home() {
         {/* // !Row 2 TitleCarousel Component (normal) // */}
         <RowSectionContainer changeClass={styles.columnReverse} jsxContent={
           <TitleCarousel
-            images={['../img/home-page/MongoDB.svg', '../img/home-page/ExpressJS.svg', '../img/home-page/ReactJS.svg', '../img/home-page/NodeJS.svg', '../img/home-page/Apollo.svg', '../img/home-page/GraphQL.svg']}
+            images={[
+              '../img/home-page/MongoDB.svg',
+              '../img/home-page/ExpressJS.svg',
+              '../img/home-page/ReactJS.svg',
+              '../img/home-page/NodeJS.svg',
+              '../img/home-page/Apollo.svg',
+              '../img/home-page/GraphQL.svg'
+            ]}
+            links={[
+              'https://www.mongodb.com/what-is-mongodb',
+              'https://expressjs.com/',
+              'https://legacy.reactjs.org/',
+              'https://nodejs.org/en/about',
+              'https://www.apollographql.com/docs/apollo-server/',
+              'https://graphql.org/'
+            ]}
             titleText={`Full-Stack Web Development`}
             disclaimerText={`For websites and applications that require dynamic and flexible data retrieval`}
           />
@@ -37,7 +52,20 @@ export default function Home() {
         {/* // !Row 3 TitleCarousel Component (row-reverse) // */}
         <RowSectionContainer changeClass={`${styles.rowReverse} ${styles.columnReverse}`} jsxContent={
           <TitleCarousel reverseText={styles.reverseText}
-            images={['../img/home-page/html5.svg', '../img/home-page/css3.svg', '../img/home-page/ReactJS.svg', '../img/home-page/javascript.svg', '../img/home-page/jQuery.svg']}
+            images={[
+              '../img/home-page/html5.svg',
+              '../img/home-page/css3.svg',
+              '../img/home-page/ReactJS.svg',
+              '../img/home-page/javascript.svg',
+              '../img/home-page/jQuery.svg'
+            ]}
+            links={[
+              'https://blog.hubspot.com/blog/tabid/6307/bid/5847/a-marketer-s-guide-to-html5.aspx',
+              'https://www.geeksforgeeks.org/difference-between-css-and-css3/',
+              'https://legacy.reactjs.org/',
+              'https://www.w3schools.com/whatis/whatis_js.asp',
+              'https://jquery.com/'
+            ]}
             titleText={`Front-End Web Development`}
             disclaimerText={`For landing pages, portfolios, and other websites that do not require a back-end server`}
           />
@@ -46,7 +74,20 @@ export default function Home() {
         {/* // !Row 4 Title Carousel Component (normal) // */}
         <RowSectionContainer changeClass={styles.columnReverse} jsxContent={
           <TitleCarousel
-            images={['../img/home-page/mySQL.svg', '../img/home-page/sql.svg', '../img/home-page/figma.svg', '../img/home-page/vercel.svg', '../img/home-page/ghPages.svg']}
+            images={[
+              '../img/home-page/mySQL.svg',
+              '../img/home-page/sql.svg',
+              '../img/home-page/figma.svg',
+              '../img/home-page/vercel.svg',
+              '../img/home-page/ghPages.svg'
+            ]}
+            links={[
+              'https://dev.mysql.com/doc/refman/8.3/en/what-is-mysql.html',
+              'https://aws.amazon.com/what-is/sql/',
+              'https://www.figma.com/',
+              'https://vercel.com/blog/what-is-vercel',
+              'https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages'
+            ]}
             titleText={`Additional Tools`}
             disclaimerText={`Additional technology to suit your needs`}
           />
