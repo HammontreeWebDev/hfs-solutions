823cf48d6515e29c69688181439b07dfe3e5f7c1
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-30 15:17:33 -0400

    bio page complete

--- pages/biography.js
+++ pages/biography.js
@@ -28,14 +28,19 @@ export default function Biography() {
                             imgHeight={590}
                             imgWidth={574}
                             jsxContent={
-                                <h6>
-                                    <span className={styles.adjective}>
-                                        <Typewriter
-                                            words={['Owner ', 'Developer ', 'Consultant ', 'Strategist ', 'Student ']}
-                                            loop={true}
-                                        />
+                                <h6 className={styles.heading}>
+                                    <span className={styles.adjectiveContainer}>
+                                        <span className={styles.adjective}>
+                                            <Typewriter
+                                                words={['Owner ', 'Developer ', 'Consultant ', 'Strategist ', 'Student ']}
+                                                loop={true}
+                                                typeSpeed={100}
+                                                deleteSpeed={80}
+                                                delaySpeed={2500}
+                                            />
+                                        </span>
                                     </span>
-                                    | Hammontree Full-Stack Solutions
+                                    Hammontree Full-Stack Solutions
                                 </h6>
                             }
                         />
@@ -47,14 +52,14 @@ export default function Biography() {
                 <CenterRowSectionContainer
                     jsxContent={
                         <SimpleCard
-                        titleText={'My Story'}
-                        p1={'First of all, I want to thank you for taking the time out of your day to visit my website. I hope that you enjoy yourself while you are here, and that you find what you are looking for.'}
-                        p2={`My journey began in 2021 when I graduated from The University Of Central Florida's Cyber Defense Professional Certificate Program. We had just spent the last 10 months learning concepts like computer networking, network security, system administration, and ethical hacking. I enjoyed my education, however, I realized that cybersecurity was not where my true passion lies. One of the courses in this program was for Python, a back-end programming language. It opened my eyes to all of the cool things that programming had to offer, and so I began building silly text-based games that only worked in the command-line-interface and unashamedly showed them to all my friends and family.`}
-                        p3={`In 2022, I decided to join the Full-Stack Web Development Boot Camp with The University Of Central Florida. I graduated with an A+ (GPA 3.93) in December 2022.`}
-                        p4={`In June 2023, I founded Hammontree Full-Stack Solutions LLC, so that I could share my passion with you.`}
-                        p5={`I'm looking forward to hearing from you!`}
-                        p6={`Sincerely,`}
-                        p7={`Kaileb Hammontree`}
+                            titleText={'My Story'}
+                            p1={'First of all, I want to thank you for taking the time out of your day to visit my website. I hope that you enjoy yourself while you are here, and that you find what you are looking for.'}
+                            p2={`My journey began in 2021 when I graduated from The University Of Central Florida's Cyber Defense Professional Certificate Program. We had just spent the last 10 months learning concepts like computer networking, network security, system administration, and ethical hacking. I enjoyed my education, however, I realized that cybersecurity was not where my true passion lies. One of the courses in this program was for Python, a back-end programming language. It opened my eyes to all of the cool things that programming had to offer, and so I began building silly text-based games that only worked in the command-line-interface and unashamedly showed them to all my friends and family.`}
+                            p3={`In 2022, I decided to join the Full-Stack Web Development Boot Camp with The University Of Central Florida. I graduated with an A+ (GPA 3.93) in December 2022.`}
+                            p4={`In June 2023, I founded Hammontree Full-Stack Solutions LLC, so that I could share my passion with you.`}
+                            p5={`I'm looking forward to hearing from you!`}
+                            p6={`Sincerely,`}
+                            p7={`Kaileb Hammontree`}
                         />
                     }
                 />
