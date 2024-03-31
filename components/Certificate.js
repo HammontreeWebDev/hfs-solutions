d02a7ad0da81a65989f1f7de489c8f7194b1dbb0
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-31 15:53:58 -0400

    certificate component and css created and ready to be used

--- components/Certificate.js
+++ components/Certificate.js
@@ -1,1 +1,42 @@
 import styles from "../styles/Certificate.module.css";
+
+import Image from 'next/image';
+
+export default function Project({ imgSrc, imgHeight, imgWidth, reverseText, titleText, verifyHref, verifyLinkText }) {
+    return (
+
+        <>
+            {/* Mock Ups & Btn */}
+
+            <div className={`column`}>
+                <Image
+                    className={styles.image}
+                    src={imgSrc}
+                    height={imgHeight}
+                    width={imgWidth}
+                />
+
+                <div className={styles.linksRow}>
+
+                    <a
+                        className={styles.links}
+                        href={verifyHref}
+                    >
+                        {verifyLinkText}
+                    </a>
+
+                </div>
+            </div>
+
+            {/* Title and sub-text */}
+            <div className={`column`}>
+                <div className={`${styles.titleContainer} titleText ${reverseText}`}>
+                    <h1>{titleText}</h1>
+                </div>
+
+            </div>
+        </>
+
+
+    )
+}
