abde4b71b040ad7b02fc435177aeadc2cc96e189
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-03-31 14:52:10 -0400

    implemented and completed project component

--- components/Project.js
+++ components/Project.js
@@ -1,1 +1,64 @@
 import styles from '../styles/Project.module.css';
+import Image from 'next/image';
+
+export default function Project({ imgSrc, imgHeight, imgWidth, reverseText, titleText, disclaimerText, additionalText, visitHref, visitLinkText, repoHref, repoLinkText }) {
+    return (
+
+        <>
+            {/* Mock Ups & Btn */}
+
+            <div className={`column`}>
+                <Image
+                    className={styles.image}
+                    src={imgSrc}
+                    height={imgHeight}
+                    width={imgWidth}
+                />
+
+                <div className={styles.linksRow}>
+
+                    <a
+                        className={styles.links}
+                        href={visitHref}
+                    >
+                        {visitLinkText}
+                    </a>
+
+                    <a
+                        className={styles.links}
+                        href={repoHref}
+                    >
+                        {repoLinkText}
+                    </a>
+
+                </div>
+            </div>
+
+            {/* Title and sub-text */}
+            <div className={`column`}>
+                <div className={`${styles.topContainer} titleText ${reverseText}`}>
+                    <h1>{titleText}</h1>
+                </div>
+
+                <div className={`${styles.bottomContainer} disclaimerText ${reverseText}`}>
+                    <p>
+
+                        {disclaimerText}
+
+                        <br />
+
+                        <span className={styles.spanText}>
+
+                            {additionalText}
+
+                        </span>
+
+                    </p>
+                </div>
+
+            </div>
+        </>
+
+
+    )
+}
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,6 +2,23 @@ import Head from "next/head";
 import styles from '../styles/Portfolio.module.css';
 import RowSectionContainer from "../components/RowSectionContainer";
 import TitleImage from "../components/TitleImage";
+import Project from "../components/Project";
+
+// jsxContent={
+//     <Project 
+//     imgSrc={} 
+//     imgHeight={} 
+//     imgWidth={} 
+//     visitHref={}
+//     visitLinkText={}
+//     repoHref={}
+//     repoLinkText={}
+//     titleText={} 
+//     disclaimerText={}
+//     additionalText={}
+//     reverseText={} 
+//     />
+// }
 
 
 export default function Portfolio() {
@@ -43,6 +60,20 @@ export default function Portfolio() {
                 {/* //* Jesse Ryder Brown Foundation */}
                 <RowSectionContainer
                     changeClass={styles.columnReverse}
+                    jsxContent={
+                        <Project 
+                        imgSrc={'/img/portfolio-page/jesseMockUp.svg'} 
+                        imgHeight={658} 
+                        imgWidth={876} 
+                        visitHref={'https://www.j-boocustoms.org/'}
+                        visitLinkText={'Visit Site'}
+                        repoHref={'https://github.com/HammontreeWebDev/j-boo-customs-nextJS'}
+                        repoLinkText={'Repository'}
+                        titleText={'Jesse Ryder Brown Foundation, Inc.'} 
+                        disclaimerText={'Built With:'}
+                        additionalText={'ReactJS | NextJS | Node.js | CSS | Figma'}
+                        />
+                    }
                 />
 
                 {/* //! Row 3 Project Component ROW REVERSE (change to column reverse at smaller screens)*/}
