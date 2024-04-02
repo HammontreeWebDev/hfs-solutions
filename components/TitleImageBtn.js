ae6b67f577728d024aeb2db11cdfecbf0f611748
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-02 16:47:47 -0400

    added skeleton to titleImageBtn component

--- components/TitleImageBtn.js
+++ components/TitleImageBtn.js
@@ -1,7 +1,12 @@
 import styles from '../styles/TitleImageBtn.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
+import Skeleton from "@mui/material/Skeleton";
 
-export default function TitleImageBtn({ titleText, disclaimerText, imgSrc, imgHeight, imgWidth, btnText, secondText, href, imgAlt}) {
+export default function TitleImageBtn({ titleText, disclaimerText, imgSrc, imgHeight, imgWidth, btnText, secondText, href, imgAlt }) {
+
+    const [loading, setLoading] = useState(true);
+
     return (
 
         <>
@@ -23,19 +28,39 @@ export default function TitleImageBtn({ titleText, disclaimerText, imgSrc, imgHe
             {/* Right Side: Image */}
             <div className={`column`}>
 
-                <Image
-                    className={styles.image}
-                    src={imgSrc}
-                    height={imgHeight}
-                    width={imgWidth}
-                    alt={imgAlt}
-                />
+                {/* //! Skeleton for image */}
+
+                {
+                    loading ?
+
+                        <Skeleton variant="rectangular">
+                            <Image
+                                className={styles.image}
+                                src={imgSrc}
+                                height={imgHeight}
+                                width={imgWidth}
+                                alt={imgAlt}
+                                onLoad={() => setLoading(false)}
+                                onLoadedData={() => setLoading(false)}
+                            />
+                        </Skeleton>
+
+                        :
+
+                        <Image
+                            className={styles.image}
+                            src={imgSrc}
+                            height={imgHeight}
+                            width={imgWidth}
+                            alt={imgAlt}
+                        />
+                }
 
                 {/* // * add button Here: */}
 
-                <a 
-                className={styles.btn}
-                href={href}
+                <a
+                    className={styles.btn}
+                    href={href}
                 >
                     {btnText}
                 </a>
